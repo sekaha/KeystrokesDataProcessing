a9fac2f57aabfc1352e0889dd97dc7edc160a198
Hakase <davidsommerfield.zg@gmail.com>
2024-01-29 23:00:29 -0600

    switched to non-pandas approach

--- renamer.sh
+++ renamer.sh
@@ -1,6 +1,5 @@
-# Rename the files to just their number
-# $(find . -type f -name "*_keystrokes.txt" -printf "%f\n")
-for FILE in *; do
+# Rename the files to just their number, we use find to avoid accidentally overwriting files we don't want to
+for FILE in $(find . -type f -name "*_keystrokes.txt" -printf "%f\n"); do
 	NEW_NAME=$(echo "$FILE" | sed 's/_.*//') # keep only number
 	NEW_NAME=$(printf "%06d" "$NEW_NAME") # format it to have leading zeros
 	NEW_NAME="${NEW_NAME}.txt" # save it as a textfile
--- session_splitter.py
+++ session_splitter.py
@@ -7,18 +7,43 @@ ids = participants["PARTICIPANT_ID"]
 
 for ID in participants["PARTICIPANT_ID"]:
     participant_file_name = "files/" + str(ID).zfill(6)
-    print(participant_file_name)
+
+    if not os.path.exists(participant_file_name + ".txt"):
+        print(participant_file_name + ".txt", "does not exist")
+        continue
+
+    print(participant_file_name + ".txt")
+    test_data = pd.read_csv(participant_file_name + ".txt", sep="\t", quoting=3)
+
+    # make a folder to session files in
+    if not os.path.exists(participant_file_name):
+        os.makedirs(participant_file_name)
+
+    session_id = ""
+    session_file = None
 
     with open(participant_file_name + ".txt") as file:
-        test_data = pd.read_csv(file, sep="\t")
+        # skip header
+        file.readline()
 
-        # make a folder to session files in
-        if not os.path.exists(participant_file_name):
-            os.makedirs(participant_file_name)
+        for l in file:
+            data = l.split("\t")
 
-        for section_id in test_data["TEST_SECTION_ID"].unique():
-            section = test_data[(test_data["TEST_SECTION_ID"] == section_id)]
+            # make a new file for each session
+            if session_id != data[1]:
+                session_id = data[1]
+                session_file = open(f"{participant_file_name}/{session_id}.txt", "w")
 
-            section.to_csv(
-                participant_file_name + f"/{section_id}.txt", sep="\t", index=False
-            )
+            # super jank fix because they included tabs as a data point :face_palm:
+            if len(data) > 10:
+                data = data[:8] + ["Tab"] + data[-1]
+
+            session_file.write("\t".join(data))
+
+    print(1 / 0)
+    """for section_id in test_data["TEST_SECTION_ID"].unique():
+        section = test_data[(test_data["TEST_SECTION_ID"] == section_id)]
+
+        section.to_csv(
+            participant_file_name + f"/{section_id}.txt", sep="\t", index=False
+        )"""
